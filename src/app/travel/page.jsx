5fcd755868fb71403c0b5eb968a97984c31410db
Ananya54321 <ananyapappula521@gmail.com>
2025-02-05 23:26:16 +0530

    travel details

---
+++ src/app/travel/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TourDialog from "@/components/pages/tourDialog";
+function page() {
+  return (
+    <div>
+      <TourDialog />
+    </div>
+  );
+}
+
+export default page;
