0b0946bf5e08a70ef0387824a8336146af790ce0
Ananya54321 <ananyapappula521@gmail.com>
2025-02-06 19:57:05 +0530

    navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,8 @@
 import Navbar from "@/components/navbar";
 export default function Home() {
   return (
-    <div className="h-screen">
-      <div className="h-[100%]">
-        <Navbar />
-      </div>
+    <div>
+      <div className="h-[100%]">{/* <Navbar /> */}</div>
       <div>
         <h1>Hello World</h1>
       </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,39 @@
 import React from "react";
-import { FaHome, FaPlane, FaLifeRing, FaUsers, FaUser } from "react-icons/fa";
+import {
+  FaHome,
+  FaPlane,
+  FaLifeRing,
+  FaUsers,
+  FaUser,
+  FaMapMarkerAlt,
+} from "react-icons/fa";
+import { IoLogoWechat } from "react-icons/io5";
 
 function Navbar() {
   return (
-    <div className="flex md:flex-col  justify-around items-center bg-[#dc2446] text-white p-2 w-full md:w-[12%] fixed bottom-0 left-0 md:relative md:bottom-auto md:top-0 md:pl-4 md:h-full">
-      <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
-        <FaHome className="mx-auto  text-2xl" />
-      </div>
-      <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
-        <FaPlane className="mx-auto  text-2xl" />
-      </div>
-      <div className="w-full flex-1 text-center md:text-left  flex items-center  md:flex-row md:gap-4">
-        <p className="mx-auto  font-black text-xl rounded-full ">SOS</p>
-      </div>
-      <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
-        <FaUsers className="mx-auto  text-3xl" />
-      </div>
-      <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
-        <FaUser className="mx-auto  text-2xl" />
+    <div className="navbar h-[100%] flex flex-colitems-center">
+      <div className="flex md:flex-col  justify-around items-center bg-[#dc2446] text-white p-2 w-full md:w-[75px] fixed bottom-0 left-0 md:relative md:bottom-auto md:top-0 md:pl-4 md:h-[90%]">
+        <div className=" w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
+          <FaHome className="mx-auto  text-2xl" />
+        </div>
+        <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
+          <FaPlane className="mx-auto  text-2xl" />
+        </div>
+        <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
+          <FaMapMarkerAlt className="mx-auto  text-2xl" />
+        </div>
+        <div className="w-full flex-1 text-center md:text-left  flex items-center  md:flex-row md:gap-4">
+          <p className="mx-auto  font-black text-xl rounded-full ">SOS</p>
+        </div>
+        <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
+          <IoLogoWechat className="mx-auto  text-4xl" />
+        </div>
+        <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
+          <FaUsers className="mx-auto  text-3xl" />
+        </div>
+        <div className="w-full flex-1 text-center md:text-left p-2 flex items-center  md:flex-row md:gap-4">
+          <FaUser className="mx-auto  text-2xl" />
+        </div>
       </div>
     </div>
   );
